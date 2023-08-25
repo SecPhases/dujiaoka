a17f66989084d30ebb32da99a13e8ff51ebd20fd
SecPhases <values@matsuri.cc>
2023-08-26 00:00:33 +0800

    测试luna模板弹窗点击关闭

--- resources/views/luna/layouts/_script.blade.php
+++ resources/views/luna/layouts/_script.blade.php
@@ -6,6 +6,20 @@
         mobile_order : '{{ __('luna.mobile_order') }}'
     };
 </script>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+    // 获取弹出型div和覆盖层的引用
+    var popupDiv = document.getElementById("layui-layer1");
+    var overlayDiv = document.getElementById("layui-layer-shade1");
+
+    // 添加点击事件监听器
+    overlayDiv.addEventListener("click", function () {
+        // 删除弹出型div和覆盖层
+        popupDiv.remove();
+        overlayDiv.remove();
+    });
+    });
+</script>
 <script src="/assets/luna/layui/layui.js"></script>
 <script src="/assets/luna/js/jquery-3.4.1.min.js"></script>
 <script src="/assets/luna/main.js"></script>
